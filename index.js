1060b3bb81e20a45c3a533488b632a38f394856a
Prischenko Sergey <jonkofee@icloud.com>
2018-04-17 00:17:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import localForage from "localforage"
+
+export default adapter => {
+  var storage = localForage.createInstance({
+    name: "axios-stack"
+  })
+
+  storage.setDriver(localForage.LOCALSTORAGE)
+
+  return config => {
+    config.timeout = 5000
+
+    function storeRequest(data) {
+      storage.setItem(String(Date.now()), data)
+    }
+
+    function removeRequest(time) {
+      storage.removeItem(time)
+    }
+
+    function sendAllRequest() {
+      storage.iterate((data, time) => {
+        adapter(data)
+          .then(removeRequest(time))
+      })
+    }
+
+    let response = adapter(config)
+      .catch(err => {
+        let {
+          code,
+          message
+        } = err
+
+        if (code === 'ECONNABORTED' || message === 'Network Error') {
+          storeRequest(config)
+        } else {
+          sendAllRequest()
+        }
+
+        return Promise.reject(err)
+      })
+      .then(resolve => {
+        sendAllRequest()
+
+        return Promise.resolve(resolve)
+      })
+
+    return response
+  }
+}
