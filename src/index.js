d6285f99d34f8537ea9f8f0b40e6397040938eda
Prischenko Sergey <jonkofee@icloud.com>
2018-04-17 17:36:35 +0300

    :recycle: oop

--- src/index.js
+++ src/index.js
@@ -1,48 +1,61 @@
-import localforage from "./plugins/localforage"
+import localForage from "localforage"
 
-export default defaultAdapter => {
-  let storage = localforage();
+export default class AxiosOffline {
+  constructor({
+    defaultAdapter,
+    storageName = "axios-stack",
+    storageDriver = localForage.LOCALSTORAGE
+  } = {}) {
+    let storageInstance = localForage.createInstance({
+      name: storageName
+    })
+    storageInstance.setDriver(storageDriver)
 
-  return config => {
-    config.timeout = config.timeout || 5000
+    this.storage        = storageInstance
+    this.defaultAdapter = defaultAdapter
+  }
+  getAdapter() {
+    return config => {
+      config.timeout = config.timeout || 5000
 
-    function storeRequest(data) {
-      storage.setItem(String(Date.now()), data)
-    }
+      let storeRequest = (data) => {
+        this.storage.setItem(String(Date.now()), data)
+      }
 
-    function removeRequest(time) {
-      storage.removeItem(time)
-    }
+      let removeRequest = (time) => {
+        this.storage.removeItem(time)
+      }
 
-    function sendAllRequest() {
-      storage.iterate((data, time) => {
-        defaultAdapter(data)
-          .then(() => removeRequest(time))
-      })
-    }
+      let sendAllRequest = () => {
+        this.storage.iterate((data, time) => {
+          this.defaultAdapter(data)
+            .then(() => removeRequest(time))
+        })
+      }
 
-    let response = defaultAdapter(config)
-      .catch(err => {
-        let {
-          code,
-          message,
-          response
-        } = err
+      let response = this.defaultAdapter(config)
+        .catch(err => {
+          let {
+            code,
+            message,
+            response
+          } = err
 
-        if (response === undefined && (code === 'ECONNABORTED' || message === 'Network Error')) {
-          storeRequest(config)
-        } else {
+          if (response === undefined && (code === 'ECONNABORTED' || message === 'Network Error')) {
+            storeRequest(config)
+          } else {
+            sendAllRequest()
+          }
+
+          return Promise.reject(err)
+        })
+        .then(resolve => {
           sendAllRequest()
-        }
 
-        return Promise.reject(err)
-      })
-      .then(resolve => {
-        sendAllRequest()
+          return Promise.resolve(resolve)
+        })
 
-        return Promise.resolve(resolve)
-      })
-
-    return response
+      return response
+    }
   }
 }
